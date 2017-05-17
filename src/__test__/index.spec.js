bec23ed321a00a0682965d3c988afb1ec8fa6c85
xvie <jansuroviak@gmail.com>
2017-05-17 16:42:49 +0200

    Initial commit

--- src/__test__/index.spec.js
+++ src/__test__/index.spec.js
@@ -1,5 +1,42 @@
-import index from '../index';
+import { render } from 'enzyme';
+import Radium from 'radium';
+import React from 'react';
+import StyledComponent from '../index';
 
-it('should return function', () => {
-  expect(typeof index).toEqual('function');
+
+@Radium
+class Button extends StyledComponent {
+  render() {
+    const style = this.getStyle();
+    return (
+      <button style={style.button}>
+        <span style={style.text}>Text</span>
+      </button>
+    );
+  }
+
+  static style = {
+    button: {
+      paddingLeft: '20px'
+    },
+    text: {
+      color: 'red'
+    }
+  }
+}
+
+it('should render Button with default style', () => {
+  expect(render(<Button />)).toMatchSnapshot();
+});
+
+it('should render Button with default style overriden with custom style (as object)', () => {
+  expect(render(<Button style={{ button: { paddingLeft: '100px' } }} />)).toMatchSnapshot();
+});
+
+it('should render Button with default style merged with custom style (as object)', () => {
+  expect(render(<Button style={{ text: { textTransform: 'uppercase' } }} />)).toMatchSnapshot();
+});
+
+it('should render Button with default style merged with custom style (as array of objects)', () => {
+  expect(render(<Button style={[{ button: { textTransform: 'uppercase' } }, { button: { marginLeft: '100px' } }, { text: { color: 'blue' } }]} />)).toMatchSnapshot();
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-export default () => {
-  console.log('new js package'); // eslint-disable-line no-console
-};
+import { PureComponent, PropTypes as RPT } from 'react';
+import { fromJS } from 'immutable';
+
+export default class StyledComponent extends PureComponent {
+  static propTypes = {
+    style: RPT.oneOfType([RPT.func, RPT.object])
+  }
+
+  static defaultProps = {
+    style: {}
+  }
+
+  getStyle(localStyle) {
+    const { style } = this.props;
+    const defaultStyle = localStyle || this.constructor.style;
+
+    if (!style) {
+      return defaultStyle;
+    }
+
+    if (style instanceof Array) {
+      const styleObj = style.reduce((acc, current) => fromJS(acc).mergeDeep(fromJS(current)), {});
+      return fromJS(defaultStyle).mergeDeep(styleObj).toJS();
+    }
+
+    return fromJS(defaultStyle).mergeDeep(fromJS(style)).toJS();
+  }
+}
