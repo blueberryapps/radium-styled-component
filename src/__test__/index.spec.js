9ab4910853970671524903850a21e9f8188c4379
xvie <jansuroviak@gmail.com>
2017-05-18 13:37:11 +0200

    New version with style constants support & removed immutable

--- src/__test__/index.spec.js
+++ src/__test__/index.spec.js
@@ -1,8 +1,53 @@
 import { render } from 'enzyme';
 import Radium from 'radium';
 import React from 'react';
-import StyledComponent from '../index';
+import StyledComponent, { deepMerge, getStyle, memoizeGetStyle } from '../index';
 
+it('should merge objects', () => {
+  const a = { a: { b: 1, c: 2 } };
+  const b = { a: { b: 5 }, d: 3 };
+  expect(deepMerge(a, b)).toEqual({ a: { b: 5, c: 2 }, d: 3 });
+});
+
+it('should merge objects 2', () => {
+  const a = { a: { b: 1, c: 2 } };
+  const b = { a: { b: 5, j: { g: 7 } }, d: 3 };
+  expect(deepMerge(a, b)).toEqual({ a: { b: 5, c: 2, j: { g: 7 } }, d: 3 });
+});
+
+it('should merge objects 3', () => {
+  const a = { a: { b: 1, c: 2 } };
+  const b = { a: { b: 5, c: { g: 7 } }, d: 3 };
+  expect(deepMerge(a, b)).toEqual({ a: { b: 5, c: { g: 7 } }, d: 3 });
+});
+
+it('should merge objects 4', () => {
+  const b = { a: { b: 1, c: 2 } };
+  const a = { a: { b: 5, c: { g: 7 } }, d: 3 };
+  expect(deepMerge(a, b)).toEqual({ a: { b: 1, c: 2 }, d: 3 });
+});
+
+it('should merge objects 5', () => {
+  const b = { a: { b: 1, c: 2 } };
+  const a = { a: { b: 5, c: { g: 7 } }, d: 3 };
+  const c = { g: 17 };
+  expect(deepMerge(a, b, c)).toEqual({ a: { b: 1, c: 2 }, d: 3, g: 17 });
+});
+
+it('memoized gets style for large objects is faster', () => {
+  const dateFirst = new Date();
+  for (let i = 0; i < 10000; i += 1) {
+    getStyle({ b: { c: 3, g: { b: { c: 3, g: { b: { c: 3 }, g: { b: { c: 3 } }, h: { b: { c: 3 }, g: { b: { c: 3 } } } } } } } }, { b: { c: 3, g: { b: { c: 3, g: { b: { c: 3 }, g: { b: { c: 3 } }, h: { b: { c: 3 }, g: { b: { c: 3 } } } } } } } }, { b: { c: 3, g: { b: { c: 3, g: { b: { c: 3 }, g: { b: { c: 3 } }, h: { b: { c: 3 }, g: { b: { c: 3 } } } } } } } }, { b: { c: 3 } }, { b: { c: 3 } });
+  }
+  const withoutMemoize = new Date() - dateFirst;
+
+  const dateSecond = new Date();
+  for (let i = 0; i < 10000; i += 1) {
+    memoizeGetStyle({ b: { c: 3, g: { b: { c: 3, g: { b: { c: 3 }, g: { b: { c: 3 } }, h: { b: { c: 3 }, g: { b: { c: 3 } } } } } } } }, { b: { c: 3, g: { b: { c: 3, g: { b: { c: 3 }, g: { b: { c: 3 } }, h: { b: { c: 3 }, g: { b: { c: 3 } } } } } } } }, { b: { c: 3, g: { b: { c: 3, g: { b: { c: 3 }, g: { b: { c: 3 } }, h: { b: { c: 3 }, g: { b: { c: 3 } } } } } } } }, { b: { c: 3 } }, { b: { c: 3 } });
+  }
+  const withMemoize = new Date() - dateSecond;
+  expect(withMemoize).toBeLessThan(withoutMemoize);
+});
 
 @Radium
 class Button extends StyledComponent {
@@ -39,4 +84,8 @@ it('should render Button with default style merged with custom style (as object)
 
 it('should render Button with default style merged with custom style (as array of objects)', () => {
   expect(render(<Button style={[{ button: { textTransform: 'uppercase' } }, { button: { marginLeft: '100px' } }, { text: { color: 'blue' } }]} />)).toMatchSnapshot();
+});
+
+it('should render Button with default style merged with custom style (as array of objects)', () => {
+  expect(render(<Button style={[{ button: { textTransform: 'uppercase' } }, () => ({ button: { marginLeft: '100px' } }), { text: { color: 'blue' } }]} />)).toMatchSnapshot();
 });
--- src/index.js
+++ src/index.js
@@ -1,28 +1,62 @@
 import { PureComponent, PropTypes as RPT } from 'react';
-import { fromJS } from 'immutable';
+import memoize from 'fast-memoize';
 
-export default class StyledComponent extends PureComponent {
-  static propTypes = {
-    style: RPT.oneOfType([RPT.func, RPT.object])
+function deepMergeObject(rootAcc, element) {
+  if (typeof element === 'string' || typeof element === 'number') {
+    return element;
   }
 
-  static defaultProps = {
-    style: {}
-  }
-
-  getStyle(localStyle) {
-    const { style } = this.props;
-    const defaultStyle = localStyle || this.constructor.style;
-
-    if (!style) {
-      return defaultStyle;
-    }
-
-    if (style instanceof Array) {
-      const styleObj = style.reduce((acc, current) => fromJS(acc).mergeDeep(fromJS(current)), {});
-      return fromJS(defaultStyle).mergeDeep(styleObj).toJS();
-    }
-
-    return fromJS(defaultStyle).mergeDeep(fromJS(style)).toJS();
-  }
+  return Object.keys(element)
+    .reduce((acc, key) => ({ ...acc, [key]: deepMergeObject(rootAcc[key] || {}, element[key]) }), rootAcc);
 }
+
+export function deepMerge(rootAcc, ...elements) {
+  return elements.reduce((acc, element) => deepMergeObject(acc, element), rootAcc);
+}
+
+export function getStyle(givenOverrideStyle, givenConstructorStyle, givenPropsStyle, constants, props) {
+  const localStyle = resolveStyleObject(givenOverrideStyle, constants, props);
+
+  const staticStyle = resolveStyleObject(givenConstructorStyle, constants, props);
+
+  const propsStyle = Array.isArray(givenPropsStyle)
+    ? deepMerge({}, ...givenPropsStyle.map(s => resolveStyleObject(s, constants, props)))
+    : resolveStyleObject(givenPropsStyle, constants, props);
+
+  return deepMerge({}, staticStyle, propsStyle, localStyle);
+}
+
+export const memoizeGetStyle = memoize(getStyle);
+
+function resolveStyleObject(functionOrObject = {}, constants, props) {
+  return typeof functionOrObject === 'function'
+    ? functionOrObject(constants, props)
+    : functionOrObject;
+}
+
+
+export function createStyledComponent(constants) {
+  return class StyledComponent extends PureComponent {
+    static propTypes = {
+      style: RPT.oneOfType([RPT.func, RPT.object])
+    }
+
+    static defaultProps = {
+      style: {}
+    }
+
+    getStyle(overrideStyle) {
+      return memoizeGetStyle(overrideStyle, this.constructor.style, this.props.style, constants, this.props);
+    }
+  };
+}
+
+const StyledComponent = createStyledComponent({});
+export default StyledComponent;
+
+
+// StyledComponent.react.js
+//
+// import {createStyledComponent} from 'styled-comonent'
+// const StyledComponent = createStyledComponent({colors: {red: '#f00'}});
+// export default StyledComponent;
