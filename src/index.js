e8fb102100168030d39220b11e83750b9a67dc71
xvie <jansuroviak@gmail.com>
2017-05-18 16:52:35 +0200

    Update version & fix object circulation

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { PureComponent, PropTypes as RPT } from 'react';
 import memoize from 'fast-memoize';
 
 function deepMergeObject(rootAcc, element) {
-  if (typeof element === 'string' || typeof element === 'number') {
+  if (typeof element === 'string' || typeof element === 'number' || typeof element === 'function') {
     return element;
   }
 
@@ -34,19 +34,21 @@ function resolveStyleObject(functionOrObject = {}, constants, props) {
     : functionOrObject;
 }
 
-
 export function createStyledComponent(constants) {
   return class StyledComponent extends PureComponent {
     static propTypes = {
+      children: RPT.node,
       style: RPT.oneOfType([RPT.func, RPT.object])
     }
 
     static defaultProps = {
+      children: null,
       style: {}
     }
 
     getStyle(overrideStyle) {
-      return memoizeGetStyle(overrideStyle, this.constructor.style, this.props.style, constants, this.props);
+      const { children, style, ...restProps } = this.props;
+      return memoizeGetStyle(overrideStyle, this.constructor.style, style, constants, restProps);
     }
   };
 }
